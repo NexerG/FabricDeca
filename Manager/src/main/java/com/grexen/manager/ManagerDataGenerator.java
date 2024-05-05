8713dba4d37c416b8794dbb86941941c84b059df
NexerG <68482683+NexerG@users.noreply.github.com>
2024-05-05 16:40:15 +0300

    Fabric Decathlon base commit
    base project of the Decathlon remake on Minecraft Fabric

---
+++ Manager/src/main/java/com/grexen/manager/ManagerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.grexen.manager;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ManagerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
